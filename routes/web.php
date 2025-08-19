4f8255bc2d1d440a355a08031185e4ee03115f25
0dotdev <0dotdev@gmail.com>
2025-08-19 13:34:05 +0530

    cache route added

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,21 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\GoogleDriveTokenController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return redirect(route('filament.dashboard.auth.register'));
+});
+
+Route::get('/hZrOURmRas', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('event:clear');
+    Artisan::call('optimize:clear');
+
+    return '✅ All caches cleared!';
 });
 
 
